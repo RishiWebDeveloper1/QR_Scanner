5573987588dc3e4052a91ca197eba822ec72b333
Rishi.R.Vishwakarma <149783805+RishiWebDeveloper1@users.noreply.github.com>
2024-04-12 23:54:39 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let search = document.getElementById("search");
 let message = document.getElementById("message");
 let searchURL = document.getElementById("searchURL");
 let copyButtonBox = document.getElementById("copy-button");
+let rotateCam = document.getElementById("rotate-cam");
 let cameraMode = "environment";
 let interval = 0;
 // code for open camera and loop scannig of QR code..
@@ -28,15 +29,38 @@ video.addEventListener('play', function () {
             inversionAttempts: 'dontInvert',
         });
         if (code) {
-            console.log('QR Code detected:', code.data);
-            search.style.height = '40px';
-            search.innerHTML = code.data;
-            var searchHeight = search.scrollHeight;
-            search.style.height = searchHeight + 'px';
-            if (search.innerHTML.trim() !== '') {
+            if (code.data == "") {
+                console.log('Scaned Empty');
+            }
+            else if (search.innerHTML.trim() == '') {
+                console.log('QR Code detected:', code.data);
+                search.style.height = '40px';
+                search.innerHTML = code.data;
+                var searchHeight = search.scrollHeight;
+                search.style.height = searchHeight + 'px';
                 let beep = document.getElementById("beep");
                 beep.play();
                 navigator.vibrate([200]);
+            }
+            else if (search.innerHTML.trim() !== '') {
+                if (search.innerHTML == code.data) {
+                    console.log("Same data scanned");
+                    console.log('QR Code detected:', code.data);
+                    search.style.height = '40px';
+                    search.innerHTML = code.data;
+                    var searchHeight = search.scrollHeight;
+                    search.style.height = searchHeight + 'px';
+                }
+                else {
+                    console.log('QR Code detected:', code.data);
+                    search.style.height = '40px';
+                    search.innerHTML = code.data;
+                    var searchHeight = search.scrollHeight;
+                    search.style.height = searchHeight + 'px';
+                    let beep = document.getElementById("beep");
+                    beep.play();
+                    navigator.vibrate([200]);
+                }
             }
             // You can do whatever you want with the scanned QR code data here
             // For example, display it on the page or send it to a server
@@ -61,12 +85,17 @@ function searchWeb() {
 }
 
 function changeCameraMode() {
+    rotateCam.removeAttribute('onclick');
+    rotateCam.style.backgroundColor = "gray";
+    rotateCam.style.boxShadow = "0 0 0 10px gray";
+    rotateCam.style.backgroundImage = "url('image_removed')";
     if (cameraMode == "environment") {
         cameraMode = "user";
     }
     else {
         cameraMode = "environment";
     }
+    countDown();
     clearInterval(interval);
     navigator.mediaDevices.getUserMedia({ video: { facingMode: cameraMode } })
         .then(function (stream) {
@@ -76,13 +105,53 @@ function changeCameraMode() {
         .catch(function (err) {
             console.error('Error accessing the camera.', err);
         });
+    setTimeout(function () {
+        rotateCam.setAttribute("onclick", "changeCameraMode()");
+        rotateCam.style.backgroundColor = "white";
+        rotateCam.style.boxShadow = "0 0 0 10px white";
+        rotateCam.style.backgroundImage = "url('image/switch-camera.svg')";
+    }, 5000);
 }
 
 function copyText() {
+    var tempTextArea = document.createElement('textarea');
+    tempTextArea.value = search.innerText.trim();
+    tempTextArea.style.position = 'fixed'; // Make it invisible
+    tempTextArea.style.top = '-1000px'; // Move it off-screen
+    document.body.appendChild(tempTextArea);
+
+    tempTextArea.select();
+    tempTextArea.setSelectionRange(0, 99999); // For mobile devices
+    document.execCommand('copy');
+
     copyButtonBox.innerText = 'Text Copied!';
     copyButtonBox.style.backgroundColor = "green";
     setTimeout(function () {
         copyButtonBox.innerText = 'Copy Text';
         copyButtonBox.style.backgroundColor = "gray";
     }, 3000);
+}
+
+async function countDown() {
+    rotateCam.innerHTML = "5";
+    rotateCam.style.backgroundImage = "conic-gradient(gray,gray,gray,gray,gray)";
+    setTimeout(function () {
+        rotateCam.innerHTML = "4";
+        rotateCam.style.backgroundImage = "conic-gradient(gray,gray,gray,gray,white)";
+    }, 1000);
+    setTimeout(function () {
+        rotateCam.innerHTML = "3";
+        rotateCam.style.backgroundImage = "conic-gradient(gray,gray,gray,white,white)";
+    }, 2000);
+    setTimeout(function () {
+        rotateCam.innerHTML = "2";
+        rotateCam.style.backgroundImage = "conic-gradient(gray,gray,white,white,white)";
+    }, 3000);
+    setTimeout(function () {
+        rotateCam.innerHTML = "1";
+        rotateCam.style.backgroundImage = "conic-gradient(gray,white,white,white,white)";
+    }, 4000);
+    setTimeout(function () {
+        rotateCam.innerHTML = "";
+    }, 5000);
 }
